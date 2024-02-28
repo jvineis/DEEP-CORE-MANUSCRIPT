51292cb35c7a1f5eda1b1131603065a6c0ba9669
Joseph Henry Vineis <jvineis@gmail.com>
2024-02-28 13:00:42 -0500

    Update create-deep-core-mapping-bowtie-mags.py

--- BINNING-and-dereplication/create-deep-core-mapping-bowtie-mags.py
+++ BINNING-and-dereplication/create-deep-core-mapping-bowtie-mags.py
@@ -10,9 +10,9 @@ for i in open(infile, 'r'):
     outfile.write("#!/bin/bash"+'\n'+
                   "#SBATCH --nodes=1"+'\n'+
                   "#SBATCH --tasks-per-node=10"+'\n'+
-                  "#SBATCH --mem=2Gb"+'\n'+
-                  "#SBATCH --time=00:45:00"+'\n'+
-                  "#SBATCH --partition=express"+'\n'+
+                  "#SBATCH --mem=100Gb"+'\n'+
+                  "#SBATCH --time=10:00:00"+'\n'+
+                  "#SBATCH --partition=short"+'\n'+
                   "#SBATCH --array=1-55"+'\n'+
                   '\n'+
                   "SAMPLE=$(sed -n "+'"'+'$'+"SLURM_ARRAY_TASK_ID"+'"'+"p x_sample-names.txt)"+'\n'+
@@ -22,7 +22,16 @@ for i in open(infile, 'r'):
                   '\n'+
                   "ref_file="+x+'\n'+
                   "fastq_file=$( echo "+'"'+"/work/jennifer.bowen/JOE/DEEP-CORE/Filtered_JGI-metaG_reads/${SAMPLE}-METAGENOME.fastq.gz"+'"'+")"+'\n'+
-                  "output_file=x_DEREPLICATED-MAPPING/"+x+"-vs-${SAMPLE}.sam"+'\n'+
+                  "output_file=${SAMPLE}/"+x+"-vs-${SAMPLE}.sam"+'\n'+
+                  "bowtie2 -x "$SAMPLE -q --interleaved ${fastq_file} -S MAPPING/${SAMPLE}.sam --threads 40
+
+                  fastq_file=$( echo "/work/jennifer.bowen/JOE/DEEP-CORE/Filtered_JGI-metaG_reads/${SAMPLE}-METAGENOME.fastq.gz")
+#bowtie2-build -f x_ALL-dereplicated-MAGs-high-quality.fa x_ALL-dereplicated-MAGs-high-quality
+#bowtie2 -x x_ALL-dereplicated-MAGs-high-quality -q --interleaved ${fastq_file} -S MAPPING/${SAMPLE}.sam --threads 40
+samtools view -bS -F 4 MAPPING/${SAMPLE}.sam > MAPPING/${SAMPLE}-FILTERED.bam
+
+anvi-init-bam MAPPING/${SAMPLE}-FILTERED.bam -o MAPPING/${SAMPLE}.bam
+anvi-profile -i MAPPING/${SAMPLE}.bam -c
                   "#covstats_file=x_DEREPLICATED-MAPPING/"+x+"-vs-${SAMPLE}-covstats.txt"+'\n'+
                   "bowtie2 -q -x ${ref_file} --interleaved ${fastq_file} -S ${output_file}"+'\n'+
                   "samtools view -F 4 -bS ${output_file} > x_DEREPLICATED-MAPPING/"+x+"-vs-${SAMPLE}-RAW.bam"+'\n'+
